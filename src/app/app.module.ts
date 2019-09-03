e460d545a6b5d5eefc0ed14542263c3c93ac613e
Bijay Yadav <bijay.yadav@cloudyfox.com>
2019-09-03 12:54:34 +0545

    implemented Form Builder (form-builder-demo.component).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppMaterialModule } from './app-material/app-material.module';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
 import { NestedFormGroupComponent } from './nested-form-group/nested-form-group.component';
+import { FormBuilderDemoComponent } from './form-builder-demo/form-builder-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameEditorComponent,
     ProfileEditorComponent,
-    NestedFormGroupComponent
+    NestedFormGroupComponent,
+    FormBuilderDemoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form-builder-demo/form-builder-demo.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-builder-demo',
+  templateUrl: './form-builder-demo.component.html',
+  styleUrls: ['./form-builder-demo.component.css']
+})
+export class FormBuilderDemoComponent implements OnInit {
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+  profileForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    })
+  })
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+}
